7fa31a834c01caa2dd528b0b7749a768f59041d0
Davi Mafra <dev@davimafra.com>
2025-12-23 20:04:24 -0300

    config: adjust Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/projeto-calculadora-investimentos/",
   plugins: [tailwindcss()],
 });
